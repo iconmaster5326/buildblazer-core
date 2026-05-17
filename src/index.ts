1902b9a229ad9a9b4aa61cd6465309730aab8036
iconmaster5326 <crazybonesj@yahoo.com>
2026-05-17 12:24:23 -0400

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import * as build from "../src/build";
-import * as counter from "../src/counter";
-import * as entity from "../src/entity";
-import * as expr from "../src/expr";
-import * as mod from "../src/mod";
-import * as stat from "../src/stat";
-import * as toggle from "../src/toggle";
-
-export default {
-  build: build,
-  counter: counter,
-  entity: entity,
-  expr: expr,
-  mod: mod,
-  stat: stat,
-  toggle: toggle,
-};
+export * from "./build";
+export * from "./counter";
+export * from "./entity";
+export * from "./expr";
+export * from "./mod";
+export * from "./stat";
+export * from "./toggle";
